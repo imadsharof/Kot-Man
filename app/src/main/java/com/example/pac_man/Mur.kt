0f5c39bf29aa7b08da8136fc731736a5715613ad
imadsharof <imadsharof@hotmail.com>
2023-03-14 14:03:56 +0100

    " Modif Map"

--- app/src/main/java/com/example/pac_man/Mur.kt
+++ app/src/main/java/com/example/pac_man/Mur.kt
@@ -26,27 +26,67 @@ class Mur(private val surfaceView: SurfaceView) {
         paintInterieur.color = Color.BLUE
         paintInterieur.strokeWidth = 10f
 
+        val paintInterieur2 = Paint()
+        paintInterieur2.color = Color.WHITE
+        paintInterieur2.strokeWidth = 10f
+
         //Dessin haut map
         canvas.drawLine(100f, 100f, w - 100f, 100f, paintInterieur)
 
-        // Dessin mur droit
-        canvas.drawLine(w - 100f, 100f, w - 100f, h/2 - 50f, paintInterieur)
-        canvas.drawLine(w - 100f, h/2 + 50f, w - 100f, h - 100f, paintInterieur)
+        /* Dessin mur droit*/
 
-        // Dessin mur bas
+            // haut
+        canvas.drawLine(w-100f,100f,w-100f,(h-200f)*1/3 + 100f,paintInterieur)
+
+            // bas
+        canvas.drawLine(w-100f,(h-200f)*2/3 + 100f , w-100f,h-100f,paintInterieur)
+
+            //sortie pacman haut
+        canvas.drawLine(w-100f,(h-200f)*1/3 + 100f,(w-200f)*9/10 + 100f,(h-200f)*1/3 + 100f,paintInterieur)
+        canvas.drawLine((w-200f)*9/10 + 100f,(h-200f)*1/3 + 100f,(w-200f)*9/10 + 100f,h/2-50f,paintInterieur)
+        canvas.drawLine(w-100f,h/2-50f,(w-200f)*9/10 + 100f,h/2-50f,paintInterieur)
+
+            //sortie pacman bas
+        canvas.drawLine(w-100f,h/2+50f,(w-200f)*9/10 + 100f,h/2+50f,paintInterieur)
+        canvas.drawLine((w-200f)*9/10 + 100f,h/2+50f,(w-200f)*9/10 + 100f,(h-200f)*2/3 + 100f,paintInterieur)
+        canvas.drawLine((w-200f)*9/10 + 100f,(h-200f)*2/3 + 100f,w-100f,(h-200f)*2/3 + 100f,paintInterieur)
+        /* Dessin mur bas*/
         canvas.drawLine(w - 100f, h - 100f, 100f, h - 100f, paintInterieur)
 
-        // Dessin mur gauche
-        canvas.drawLine(100f, h - 100f, 100f, h/2 + 50f, paintInterieur)
-        canvas.drawLine(100f, h/2 - 50f, 100f, 100f, paintInterieur)
+        /*Dessin mur gauche*/
 
-        // Dessin mur pour que pacman passe de droite a gauche
-        canvas.drawLine(w - 100f,h/2 - 50f,w ,h/2 - 50f,paintInterieur)
-        canvas.drawLine(w - 100f,h/2 + 50f,w  ,h/2 + 50f,paintInterieur)
+        // haut
+        canvas.drawLine(100f,100f,100f,(h-200f)*1/3 + 100f,paintInterieur)
+
+        // bas
+        canvas.drawLine(100f,(h-200f)*2/3 + 100f , 100f,h-100f,paintInterieur)
+
+        // sortie pacman haut
+        canvas.drawLine(100f,(h-200f)*1/3 + 100f,(w-200f)/10 + 100f,(h-200f)*1/3 + 100f,paintInterieur)
+        canvas.drawLine((w-200f)/10 + 100f ,(h-200f)*1/3 + 100f,(w-200f)/10 + 100f, h/2-50f, paintInterieur )
+        canvas.drawLine((w-200f)/10 + 100f,h/2-50f,100f,h/2-50f,paintInterieur)
+
+        //sortie pacman bas
+
+        canvas.drawLine(100f,h/2+50f,(w-200f)/10 + 100f,h/2+50f,paintInterieur)
+        canvas.drawLine((w-200f)/10 + 100f,h/2+50f,(w-200f)/10 + 100f,(h-200f)*2/3 + 100f,paintInterieur)
+        canvas.drawLine((w-200f)/10 + 100f,(h-200f)*2/3 + 100f,100f,(h-200f)*2/3 + 100f,paintInterieur)
 
         // Dessin mur pour que pacman passe de gauche à droite
-        canvas.drawLine(100f,h/2 + 50f, 0f ,h/2 + 50f,paintInterieur)
-        canvas.drawLine( 100f,h/2 - 50f, 0f ,h/2 - 50f,paintInterieur)
+
+
+
+
+
+
+        // Dessin spawn fantôme
+        canvas.drawLine(w/2-100f,h/2-50f,w/2-100f/3,h/2-50f,paintInterieur)
+        canvas.drawLine(w/2+100f/3,h/2-50f,w/2 + 100f,h/2 -50f,paintInterieur)
+
+        canvas.drawLine(w/2+100f,h/2-50f, w/2+100f,h/2+50f,paintInterieur)
+        canvas.drawLine(w/2-100f,h/2-50f, w/2-100f,h/2+50f,paintInterieur)
+
+        canvas.drawLine(w/2-100f,h/2+50f,w/2+100f,h/2+50f,paintInterieur)
 
 
 
